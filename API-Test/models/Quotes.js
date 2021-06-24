f79a6bd2741368203fc851e994b38e2ab7b1dbfa
UnknownVII <85911718+UnknownVII@users.noreply.github.com>
2021-06-24 14:01:00 +0800

    Delete API-Test directory

--- API-Test/models/Quotes.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const QuoteSchema = new mongoose.Schema({
-    content: String,
-    author: String
-});
-
-module.exports = mongoose.model('Quote', QuoteSchema);
